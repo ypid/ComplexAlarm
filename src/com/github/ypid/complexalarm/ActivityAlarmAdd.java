f2f64f73360c7cb6bbe26625e6cde45f3a0f3cbc
Robin Schneider <ypid23@aol.de>
2014-08-11 18:41:54 +0200

    Added activity to add alarms.

--- src/com/github/ypid/complexalarm/ActivityAlarmAdd.java
+++ src/com/github/ypid/complexalarm/ActivityAlarmAdd.java
@@ -1,24 +1,22 @@
 package com.github.ypid.complexalarm;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class AlarmList extends ActionBarActivity {
+public class ActivityAlarmAdd extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_alarm_list);
+        setContentView(R.layout.activity_alarm_add);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.alarm_list, menu);
+        getMenuInflater().inflate(R.menu.activity_alarm_add, menu);
         return true;
     }
 
@@ -32,9 +30,5 @@ public class AlarmList extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-    
-    public void alarmAdd(View view) {
-        startActivity(new Intent(this, ActivityAlarmAdd.class));
     }
 }
